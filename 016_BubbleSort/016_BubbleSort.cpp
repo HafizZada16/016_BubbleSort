fd42ea593e46a6d4ff6009e0cc14f08abcfc6f68
HafizZada16 <hafizzada16@gmail.com>
2024-03-07 18:54:59 +0700

    menambah void input

--- 016_BubbleSort/016_BubbleSort.cpp
+++ 016_BubbleSort/016_BubbleSort.cpp
@@ -19,7 +19,11 @@ void input() {
 	cout << "======================" << endl;
 	cout << "Masukkan  Elemen Array" << endl;
 	cout << "======================" << endl;
-
+	
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
 }
 
 id bubblesortArray() {
